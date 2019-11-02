2efc3f2e9e8510d06f48ce3789f4297070be52e3
youngychen <57090527+youngychen@users.noreply.github.com>
2019-11-03 02:06:24 +0800

    Update CurveFXSpot.cpp

--- minirisk/minirisk/src/CurveFXSpot.cpp
+++ minirisk/minirisk/src/CurveFXSpot.cpp
@@ -1,1 +1,13 @@
 #include "CurveFXSpot.h"
+#include "Market.h"
+
+
+namespace minirisk {
+  
+  CurveFXSpot::CurveFXSpot(Market *mkt, const Date& today, const std::string& curve_name)
+  
+  : m_today(today)
+  ，m_name(curve_name) 
+  {}
+ 
+} // namespace minirisk
