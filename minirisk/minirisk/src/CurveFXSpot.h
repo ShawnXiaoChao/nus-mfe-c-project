76b708d2c3d1afaaa5e31b4b6ea53829ccb32a84
youngychen <57090527+youngychen@users.noreply.github.com>
2019-11-03 02:24:50 +0800

    Update CurveFXSpot.h

--- minirisk/minirisk/src/CurveFXSpot.h
+++ minirisk/minirisk/src/CurveFXSpot.h
@@ -7,18 +7,23 @@ struct Market;
 
 struct CurveFXSpot : ICurveFXSpot
 {
+
+    
+    virtual Date today() const { return m_today; }
+
     virtual string name() const { return m_name; }
 
     CurveFXSpot(Market *mkt, const Date& today, const string& curve_name);
 
     // compute the discount factor
-    double spot(const Date& t) const;
+    double spot() const { return m_rate; }
 
-    virtual Date today() const { return m_today; }
+   
 
 private:
     Date   m_today;
     string m_name;
+    double m_rate;
 };
 
 
