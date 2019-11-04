ab0dac22a16054abde08ec7de333f3435a9cb16d
zy2380 <57090957+zy2380@users.noreply.github.com>
2019-11-04 17:44:22 +0800

    Update PortfolioUtils.h

--- minirisk/minirisk/src/PortfolioUtils.h
+++ minirisk/minirisk/src/PortfolioUtils.h
@@ -7,34 +7,36 @@
 
 namespace minirisk {
 
-struct Market;
+	struct Market;
+	// MY PART starts+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	typedef std::vector<std::pair<double, string>> portfolio_values_t;
+	typedef std::pair<double, std::vector<std::pair<size_t, string>>> compute_total_t;
+	//MY PART ends+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// get pricer for each trade
+	std::vector<ppricer_t> get_pricers(const portfolio_t& portfolio);
 
-typedef std::vector<double> portfolio_values_t;
+	// compute prices
+	portfolio_values_t compute_prices(const std::vector<ppricer_t>& pricers, Market& mkt);
 
-// get pricer for each trade
-std::vector<ppricer_t> get_pricers(const portfolio_t& portfolio);
+	// compute the cumulative book value
+	compute_total_t portfolio_total(const portfolio_values_t& values); //MY PART modified here+++++++++++++++++++++++++++++++++
 
-// compute prices
-portfolio_values_t compute_prices(const std::vector<ppricer_t>& pricers, Market& mkt);
+	// Compute PV01 (i.e. sensitivity with respect to interest rate dV/dr)
+	// Use central differences, absolute bump of 0.01%, rescale result for rate movement of 0.01%
+	std::vector<std::pair<string, portfolio_values_t>> PV01Bucketed(const std::vector<ppricer_t>& pricers, const Market& mkt); //Modified++++++++++++++++++++
+	std::vector<std::pair<string, portfolio_values_t>> PV01Parallel(const std::vector<ppricer_t>& pricers, const Market& mkt); //Modified++++++++++++++++++++
 
-// compute the cumulative book value
-double portfolio_total(const portfolio_values_t& values);
+	// save portfolio to file
+	void save_portfolio(const string& filename, const std::vector<ptrade_t>& portfolio);
 
-// Compute PV01 (i.e. sensitivity with respect to interest rate dV/dr)
-// Use central differences, absolute bump of 0.01%, rescale result for rate movement of 0.01%
-std::vector<std::pair<string, portfolio_values_t>> compute_pv01(const std::vector<ppricer_t>& pricers, const Market& mkt);
+	// load portfolio from file
+	std::vector<ptrade_t>  load_portfolio(const string& filename);
 
-// save portfolio to file
-void save_portfolio(const string& filename, const std::vector<ptrade_t>& portfolio);
+	// print portfolio to cout
+	void print_portfolio(const portfolio_t& portfolio);
 
-// load portfolio from file
-std::vector<ptrade_t>  load_portfolio(const string& filename);
-
-// print portfolio to cout
-void print_portfolio(const portfolio_t& portfolio);
-
-// print portfolio to cout
-void print_price_vector(const string& name, const portfolio_values_t& values);
+	// print portfolio to cout
+	void print_price_vector(const string& name, const portfolio_values_t& values);
 
 
 } // namespace minirisk
