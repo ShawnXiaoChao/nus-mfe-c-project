7f3663151ce52225b884944a5f6ab526fe9b36a2
zy2380 <57090957+zy2380@users.noreply.github.com>
2019-11-04 17:41:01 +0800

    Update CurveDiscount.h

--- minirisk/minirisk/src/CurveDiscount.h
+++ minirisk/minirisk/src/CurveDiscount.h
@@ -1,25 +1,31 @@
 #pragma once
 #include "ICurve.h"
+#include <vector>
+#include <map>
+
 
 namespace minirisk {
 
-struct Market;
+    struct Market;
 
-struct CurveDiscount : ICurveDiscount
-{
-    virtual string name() const { return m_name; }
+    struct CurveDiscount : ICurveDiscount
+    {
+        virtual string name() const { return m_name; }
 
-    CurveDiscount(Market *mkt, const Date& today, const string& curve_name);
+        CurveDiscount(Market *mkt, const Date& today, const string& curve_name);
 
-    // compute the discount factor
-    double df(const Date& t) const;
+        // compute the discount factor
+        double df(const Date& t) const;
 
-    virtual Date today() const { return m_today; }
+        virtual Date today() const { return m_today; }
 
-private:
-    Date   m_today;
-    string m_name;
-    double m_rate;
-};
+    private:
+        Date   m_today;
+        string m_name;
+		std::vector<Date> m_date;  //save the date points
+		std::vector<double> m_rate_t; //save the rate of correspoding date points
+	    //std::map <Date, double> m_rate; //save map to change m_rate   
+		double m_rate;
+    };
 
 } // namespace minirisk
